2ea77baf18d397dacbae188c0e01266f914eb1f3
Alvar-Cont <aconbal812@g.educaand.es>
2025-12-01 20:42:26 +0100

    añadir conexion a vue **
    no completo

---
+++ vue-project/src/components/conexionSB.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '../api/axios'  // 👈 Importas tu Axios configurado
+
+const mensaje = ref("Cargando...")
+
+onMounted(async () => {
+  try {
+    const res = await api.get("/mensaje") // 👈 ya no pones el URL completo
+    mensaje.value = res.data
+  } catch (e) {
+    mensaje.value = "Error al conectar"
+  }
+})
+</script>
+
+<template>
+  <h1>{{ mensaje }}</h1>
+</template>
