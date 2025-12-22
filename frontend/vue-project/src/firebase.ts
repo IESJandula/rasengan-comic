04b299d54149aed3492bc96cc56dc06a6715a682
Alohbasto <aconbal812@g.educaand.es>
2025-12-22 20:52:16 +0100

    fix firebase.ts

---
+++ frontend/vue-project/src/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr9eIaI5Gy4Eho5gfoXGlNdmeksa00mCQ",
+  authDomain: "rasengancomics-a81f8.firebaseapp.com",
+  projectId: "rasengancomics-a81f8",
+  storageBucket: "rasengancomics-a81f8.firebasestorage.app",
+  messagingSenderId: "12217003680",
+  appId: "1:12217003680:web:73aee41ee46dface3023c5",
+  measurementId: "G-BBF86SKDSJ"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
